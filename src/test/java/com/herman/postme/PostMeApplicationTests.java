fb3966393236d4b903b1526cfa5caa9b60caf1ea
scal3 <cool.goga501@gmail.com>
2024-01-28 15:33:38 +0100

    Basic setup

---
+++ src/test/java/com/herman/postme/PostMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herman.postme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
