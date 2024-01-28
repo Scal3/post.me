950bdce1935d4d87504543adb50086e2ce327892
scal3 <cool.goga501@gmail.com>
2024-01-28 23:10:21 +0100

    feat/create PostDto fix/disable security

--- src/main/java/com/herman/postme/post/controller/PostController.java
+++ src/main/java/com/herman/postme/post/controller/PostController.java
@@ -2,6 +2,9 @@ package com.herman.postme.post.controller;
 
 import com.herman.postme.post.dto.PostDto;
 import com.herman.postme.post.entity.Post;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
 public class PostController {
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Post> getAllPosts(@RequestParam(defaultValue = "50") int limit) {
         // There is should be all posts with comments
         // Limit is max 50 per one requests, it could be set up by request param "limit"
@@ -18,12 +22,15 @@ public class PostController {
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Post getOnePostById(@PathVariable long id) {
         return null;
     }
 
-    @PostMapping
-    public void createPost(PostDto dto) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPost(@Valid @RequestBody PostDto dto) {
         // Just make entity and save
+        System.out.println(dto);
     }
 }
--- src/main/java/com/herman/postme/security/config/AuthConfig.java
+++ src/main/java/com/herman/postme/security/config/AuthConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +12,11 @@ public class AuthConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
+        http
+                .authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
