ec45342b4b040b86946f9d14c698c92907966432
scal3 <cool.goga501@gmail.com>
2024-02-03 13:31:16 +0100

    feat/create DTO for comment

--- src/main/java/com/herman/postme/comment/dto/CommentDto.java
+++ src/main/java/com/herman/postme/comment/dto/CommentDto.java
@@ -1,4 +1,29 @@
 package com.herman.postme.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class CommentDto {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 55)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 10, max = 255)
+    private String text;
+
+    @NotNull
+    @Positive
+    private Long postId;
+
+    private LocalDateTime createdAt;
 }
