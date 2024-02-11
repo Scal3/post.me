2840b5ccb140b93969e367cd9709984908174b64
Herman <cool.goga501@gmail.com>
2024-02-11 19:25:10 +0100

    feat/add findByEmail to UserRepository and UserService

--- src/main/java/com/herman/postme/user/service/UserService.java
+++ src/main/java/com/herman/postme/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.herman.postme.user.service;
 
+import com.herman.postme.exception.exceptionimp.NotFoundException;
+import com.herman.postme.user.entity.User;
 import com.herman.postme.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,5 +14,9 @@ public class UserService {
 
     private final UserRepository repository;
 
-
+    public User findByEmail(String email) {
+        return repository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException("User with email " + email + " is not found")
+        );
+    }
 }
