f8a3726d1557a67db01b764e8c78b7c23df784fa
Herman <cool.goga501@gmail.com>
2024-02-11 21:08:57 +0100

    feat/add custom DatabaseUserDetailsService

---
+++ src/main/java/com/herman/postme/auth/service/DatabaseUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.herman.postme.auth.service;
+
+import com.herman.postme.user.entity.User;
+import com.herman.postme.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Could not findUser with email = " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getName()))
+        );
+    }
+}
