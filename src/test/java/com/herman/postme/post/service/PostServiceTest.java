42c43fe0afeb9bf636ef39799e058b59c85b42b1
Herman <cool.goga501@gmail.com>
2024-02-15 00:17:25 +0100

    upd/update PostServiceTest adding new logic

--- src/test/java/com/herman/postme/post/service/PostServiceTest.java
+++ src/test/java/com/herman/postme/post/service/PostServiceTest.java
@@ -1,169 +1,400 @@
 package com.herman.postme.post.service;
 
+import com.herman.postme.exception.exceptionimp.ForbiddenException;
 import com.herman.postme.exception.exceptionimp.NotFoundException;
-import com.herman.postme.post.dto.PostDto;
+import com.herman.postme.post.dto.*;
 import com.herman.postme.post.entity.Post;
+import com.herman.postme.post.enums.PostSortOrder;
 import com.herman.postme.post.repository.PostRepository;
+import com.herman.postme.role.entity.Role;
+import com.herman.postme.role.service.RoleService;
+import com.herman.postme.user.entity.User;
+import com.herman.postme.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 // I added H2 in maven test scope for tests
-//@SpringBootTest
-//@Transactional
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//@RequiredArgsConstructor(onConstructor_ = @Autowired)
-//class PostServiceTest {
-//
-//    private final PostService postService;
-//
-//    private final PostRepository postRepository;
-//
-//    @Test
-//    public void get_all_posts_normal_case() {
-//        List<Post> mockPosts = put5PostsIntoDBByRepository();
-//        List<Post> posts = postService.getAllPosts(0, 20);
-//
-//        assertEquals(5, posts.size());
-//        assertEquals(mockPosts.get(0).getText(), posts.get(0).getText());
-//        assertEquals(mockPosts.get(1).getText(), posts.get(1).getText());
-//        assertEquals(mockPosts.get(2).getText(), posts.get(2).getText());
-//        assertEquals(mockPosts.get(3).getText(), posts.get(3).getText());
-//        assertEquals(mockPosts.get(4).getText(), posts.get(4).getText());
-//    }
-//
-//    @Test
-//    public void get_all_posts_no_posts_case() {
-//        List<Post> posts = postService.getAllPosts(0, 20);
-//        assertEquals(0, posts.size());
-//    }
-//
-//    @Test
-//    public void get_all_posts_limit_2_but_there_is_5_posts_case() {
-//        List<Post> mockPosts = put5PostsIntoDBByRepository();
-//        List<Post> posts = postService.getAllPosts(0, 2);
-//
-//        assertEquals(2, posts.size());
-//        assertEquals(mockPosts.get(0).getText(), posts.get(0).getText());
-//        assertEquals(mockPosts.get(1).getText(), posts.get(1).getText());
-//    }
-//
-//    @Test
-//    public void get_all_posts_limit_2_page_1_case() {
-//        List<Post> mockPosts = put5PostsIntoDBByRepository();
-//        List<Post> posts = postService.getAllPosts(1, 2);
-//
-//        assertEquals(2, posts.size());
-//        assertEquals(mockPosts.get(2).getText(), posts.get(0).getText());
-//        assertEquals(mockPosts.get(3).getText(), posts.get(1).getText());
-//    }
-//
-//    @Test
-//    public void get_one_post_by_id_normal_case() {
-//        List<Post> mockPosts = put5PostsIntoDBByRepository();
-//        Post post = postService.getOnePostById(1);
-//
-//        assertEquals(mockPosts.get(4).getText(), post.getText());
-//    }
-//
-//    @Test
-//    public void get_one_post_by_id_no_posts_case() {
-//        assertThrows(NotFoundException.class, () -> {
-//            Post post = postService.getOnePostById(1);
-//        });
-//    }
-//
-//    @Test
-//    public void get_one_post_by_id_post_is_not_found_case() {
-//        assertThrows(NotFoundException.class, () -> {
-//            Post post = postService.getOnePostById(1);
-//        });
-//    }
-//
-//    @Test
-//    public void create_post_normal_case() {
-//        PostDto postDto = new PostDto();
-//        postDto.setUsername("user");
-//        postDto.setHeading("heading");
-//        postDto.setText("text");
-//
-//        postService.createPost(postDto);
-//        Post post = postService.getOnePostById(1);
-//
-//        assertEquals(postDto.getUsername(), post.getUsername());
-//        assertEquals(postDto.getHeading(), post.getHeading());
-//        assertEquals(postDto.getText(), post.getText());
-//    }
-//
-//    @Test
-//    public void create_post_no_username_in_dto_case() {
-//        PostDto postDto = new PostDto();
-//        postDto.setHeading("heading");
-//        postDto.setText("text");
-//
-//        assertThrows(DataIntegrityViolationException.class, () -> {
-//            postService.createPost(postDto);
-//        });
-//    }
-//
-//    @Test
-//    public void create_post_no_heading_in_dto_case() {
-//        PostDto postDto = new PostDto();
-//        postDto.setUsername("user");
-//        postDto.setText("text");
-//
-//        assertThrows(DataIntegrityViolationException.class, () -> {
-//            postService.createPost(postDto);
-//        });
-//    }
-//
-//    @Test
-//    public void create_post_no_text_in_dto_case() {
-//        PostDto postDto = new PostDto();
-//        postDto.setUsername("user");
-//        postDto.setHeading("heading");
-//
-//        assertThrows(DataIntegrityViolationException.class, () -> {
-//            postService.createPost(postDto);
-//        });
-//    }
-//
-//    // Each post was added in one year, month and day
-//    // Only the difference in time
-//    // Interval between posts is 1 hour, fist post was adder earlier than next
-//    // Posts were added into returning list in DESC order by date
-//    private List<Post> put5PostsIntoDBByRepository() {
-//        Post post1 = new Post("user", "post 1", "post 1",
-//                LocalDateTime.of(2023, Month.JULY, 20, 12, 0));
-//
-//        Post post2 = new Post("user", "post 2", "post 2",
-//                LocalDateTime.of(2023, Month.JULY, 20, 13, 0));
-//
-//        Post post3 = new Post("user", "post 3", "post 3",
-//                LocalDateTime.of(2023, Month.JULY, 20, 14, 0));
-//
-//        Post post4 = new Post("user", "post 4", "post 4",
-//                LocalDateTime.of(2023, Month.JULY, 20, 15, 0));
-//
-//        Post post5 = new Post("user", "post 5", "post 5",
-//                LocalDateTime.of(2023, Month.JULY, 20, 16, 0));
-//
-//        postRepository.save(post1);
-//        postRepository.save(post2);
-//        postRepository.save(post3);
-//        postRepository.save(post4);
-//        postRepository.save(post5);
-//
-//        return List.of(post5, post4, post3, post2, post1);
-//    }
-//}
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class PostServiceTest {
+
+    private static final String MOCK_USER_EMAIL = "user1@user.com";
+
+    private static final String MOCK_USER_LOGIN = "user12345";
+
+    private static final String MOCK_USER_PASSWORD = "user12345";
+
+    private static final String MOCK_POST_FIRST_HEADING = "post number 1";
+
+    private static final String MOCK_POST_SECOND_HEADING = "post number 2";
+
+    private static final String MOCK_POST_THIRD_HEADING = "post number 3";
+
+    private static final String MOCK_POST_FORTH_HEADING = "post number 4";
+
+    private static final String MOCK_POST_FIFTH_HEADING = "post number 5";
+
+    private final PostService postService;
+
+    private final RoleService roleService;
+
+    private final ModelMapper modelMapper;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    private final PostRepository postRepository;
+
+    @BeforeEach
+    public void setup(TestInfo info) {
+        boolean isExcludeSetup = info.getTags()
+                .stream()
+                .anyMatch((tag) -> tag.equals("excludeBeforeEach"));
+
+        if (isExcludeSetup) return;
+
+        Role userRole = modelMapper.map(roleService.getUserRole(), Role.class);
+
+        User mockUser = addMockUserToDB(
+                MOCK_USER_EMAIL,
+                MOCK_USER_LOGIN,
+                MOCK_USER_PASSWORD,
+                LocalDateTime.now(),
+                userRole
+        );
+
+        LocalDateTime wasCreatedFirst =
+                LocalDateTime.of(2020, Month.JULY, 20, 12, 30);
+        LocalDateTime wasCreatedSecond =
+                LocalDateTime.of(2020, Month.JULY, 21, 12, 30);
+        LocalDateTime wasCreatedThird =
+                LocalDateTime.of(2020, Month.JULY, 22, 12, 30);
+        LocalDateTime wasCreatedForth =
+                LocalDateTime.of(2020, Month.JULY, 23, 12, 30);
+        LocalDateTime wasCreatedFifth =
+                LocalDateTime.of(2020, Month.JULY, 24, 12, 30);
+
+        addMockPostsToDB(MOCK_POST_FIRST_HEADING, "post number 1", wasCreatedFirst, mockUser);
+        addMockPostsToDB(MOCK_POST_SECOND_HEADING, "post number 2", wasCreatedSecond, mockUser);
+        addMockPostsToDB(MOCK_POST_THIRD_HEADING, "post number 3", wasCreatedThird, mockUser);
+        addMockPostsToDB(MOCK_POST_FORTH_HEADING, "post number 4", wasCreatedForth, mockUser);
+        addMockPostsToDB(MOCK_POST_FIFTH_HEADING, "post number 5", wasCreatedFifth, mockUser);
+
+        setAuthenticationToMockUser(
+                MOCK_USER_EMAIL,
+                passwordEncoder.encode(MOCK_USER_PASSWORD),
+                userRole.getName()
+        );
+    }
+
+    private User addMockUserToDB(
+            String email, String login, String password, LocalDateTime createdAt, Role role
+    ) {
+        User mockUser = new User();
+        mockUser.setEmail(email);
+        mockUser.setLogin(login);
+        mockUser.setPasswordHash(passwordEncoder.encode(password));
+        mockUser.setCreatedAt(createdAt);
+        mockUser.setRole(role);
+
+        return userRepository.save(mockUser);
+    }
+
+    private Post addMockPostsToDB(String heading, String text, LocalDateTime createdAt, User user) {
+        Post post = new Post();
+        post.setHeading(heading);
+        post.setText(text);
+        post.setCreatedAt(createdAt);
+        post.setUser(user);
+
+        return postRepository.save(post);
+    }
+
+    private void setAuthenticationToMockUser(String email, String passwordHash, String roleName) {
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        passwordHash,
+                        Collections.singletonList(new SimpleGrantedAuthority(roleName))
+                );
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+    @Test
+    public void get_all_posts_date_fresher_sort_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getAllPosts(0, 15, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(5, posts.size());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FIRST_HEADING, posts.get(4).getHeading());
+    }
+
+    @Test
+    public void get_all_posts_date_older_sort_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getAllPosts(0, 15, PostSortOrder.DATE_OLDER);
+
+        assertEquals(5, posts.size());
+        assertEquals(MOCK_POST_FIRST_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(4).getHeading());
+    }
+
+    @Test
+    public void get_all_posts_page_0_limit_2_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getAllPosts(0, 2, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(2, posts.size());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FORTH_HEADING, posts.get(1).getHeading());
+    }
+
+    @Test
+    public void get_all_posts_page_1_limit_2_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getAllPosts(1, 2, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(2, posts.size());
+        assertEquals(MOCK_POST_THIRD_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_SECOND_HEADING, posts.get(1).getHeading());
+    }
+
+    @Test
+    @Tag("excludeBeforeEach")
+    public void get_all_posts_no_posts_in_db_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getAllPosts(0, 15, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(0, posts.size());
+    }
+
+    @Test
+    public void get_one_post_by_id_normal_case() {
+        PostDtoWithComments post = postService.getOnePostById(1L);
+
+        assertEquals(MOCK_POST_FIRST_HEADING, post.getHeading());
+    }
+
+    @Test
+    public void get_one_post_by_id_post_is_not_found_case() {
+        assertThrows(NotFoundException.class, () -> {
+            postService.getOnePostById(1000L);
+        });
+    }
+
+    @Test
+    public void get_users_post_by_id_date_fresher_sort_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getUsersPostById(2, 0, 15, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(5, posts.size());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FIRST_HEADING, posts.get(4).getHeading());
+    }
+
+    @Test
+    public void get_users_post_by_id_date_older_sort_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getUsersPostById(2, 0, 15, PostSortOrder.DATE_OLDER);
+
+        assertEquals(5, posts.size());
+        assertEquals(MOCK_POST_FIRST_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(4).getHeading());
+    }
+
+    @Test
+    public void get_users_post_by_id_page_0_limit_2_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getUsersPostById(2, 0, 2, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(2, posts.size());
+        assertEquals(MOCK_POST_FIFTH_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_FORTH_HEADING, posts.get(1).getHeading());
+    }
+
+    @Test
+    public void get_users_post_by_id_page_1_limit_2_case() {
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getUsersPostById(2, 1, 2, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(2, posts.size());
+        assertEquals(MOCK_POST_THIRD_HEADING, posts.get(0).getHeading());
+        assertEquals(MOCK_POST_SECOND_HEADING, posts.get(1).getHeading());
+    }
+
+    @Test
+    @Tag("excludeBeforeEach")
+    public void get_users_post_by_id_no_posts_in_db_case() {
+        Role userRole = modelMapper.map(roleService.getUserRole(), Role.class);
+
+        addMockUserToDB(
+                MOCK_USER_EMAIL,
+                MOCK_USER_LOGIN,
+                MOCK_USER_PASSWORD,
+                LocalDateTime.now(),
+                userRole
+        );
+
+        List<PostDtoWithCommentQuantity> posts =
+                postService.getUsersPostById(2, 0, 15, PostSortOrder.DATE_FRESHER);
+
+        assertEquals(0, posts.size());
+    }
+
+    @Test
+    public void get_users_post_by_id_user_is_not_found_case() {
+        assertThrows(NotFoundException.class, () -> {
+            postService.getUsersPostById(1000, 0, 15, PostSortOrder.DATE_FRESHER);
+        });
+    }
+
+    @Test
+    public void create_post_normal_case() {
+        String heading = "test heading";
+        String text = "test post's text";
+
+        CreatePostDto dto = new CreatePostDto();
+        dto.setHeading(heading);
+        dto.setText(text);
+
+        PostDto postDto = postService.createPost(dto);
+
+        assertEquals(heading, postDto.getHeading());
+        assertEquals(text, postDto.getText());
+    }
+
+    @Test
+    public void update_post_normal_case() {
+        long id = 1;
+        String heading = "Updated " + MOCK_POST_FIRST_HEADING;
+        String text = "Updated " + MOCK_POST_FIRST_HEADING;
+
+        PostDtoWithComments postForUpdate = postService.getOnePostById(id);
+
+        assertEquals(MOCK_POST_FIRST_HEADING, postForUpdate.getHeading());
+
+        UpdatePostDto dto = new UpdatePostDto();
+        dto.setId(id);
+        dto.setHeading(heading);
+        dto.setText(text);
+
+        PostDto updatedPost = postService.updatePost(dto);
+
+        assertEquals(id, updatedPost.getId());
+        assertEquals(heading, updatedPost.getHeading());
+        assertEquals(text, updatedPost.getText());
+        assertTrue(updatedPost.getIsUpdated());
+    }
+
+    @Test
+    public void update_post_post_is_not_found_case() {
+        long notFoundId = 1000;
+        String heading = "Updated post";
+        String text = "Updated posts text";
+
+        UpdatePostDto dto = new UpdatePostDto();
+        dto.setId(notFoundId);
+        dto.setHeading(heading);
+        dto.setText(text);
+
+        assertThrows(NotFoundException.class, () -> {
+            postService.updatePost(dto);
+        });
+    }
+
+    @Test
+    public void update_post_user_is_not_authorized_to_update_post_case() {
+        // Other user created a post
+        Role userRole = modelMapper.map(roleService.getUserRole(), Role.class);
+        User newUser = addMockUserToDB(
+                "newUser@gmail.com",
+                "newUserLogin",
+                "newUserPassword",
+                LocalDateTime.now(),
+                userRole
+        );
+
+        Post post = addMockPostsToDB(
+                "Simple post name",
+                "Simple post text",
+                LocalDateTime.now(),
+                newUser
+        );
+
+        // And we're trying to change it with our token
+        String heading = "Updated post";
+        String text = "Updated posts text";
+
+        UpdatePostDto dto = new UpdatePostDto();
+        dto.setId(post.getId());
+        dto.setHeading(heading);
+        dto.setText(text);
+
+        assertThrows(ForbiddenException.class, () -> {
+            postService.updatePost(dto);
+        });
+    }
+
+    @Test
+    public void delete_post_normal_case() {
+        postService.deletePost(1);
+
+        assertThrows(NotFoundException.class, () -> {
+            postService.getOnePostById(1);
+        });
+    }
+
+    @Test
+    public void delete_post_post_is_not_found_case() {
+        assertThrows(NotFoundException.class, () -> {
+            postService.deletePost(1000);
+        });
+    }
+
+    @Test
+    public void delete_post_user_is_not_authorized_to_delete_post_case() {
+        // Other user created a post
+        Role userRole = modelMapper.map(roleService.getUserRole(), Role.class);
+        User newUser = addMockUserToDB(
+                "newUser@gmail.com",
+                "newUserLogin",
+                "newUserPassword",
+                LocalDateTime.now(),
+                userRole
+        );
+
+        Post post = addMockPostsToDB(
+                "Simple post name",
+                "Simple post text",
+                LocalDateTime.now(),
+                newUser
+        );
+
+        // And we're trying to delete it with our token
+        assertThrows(ForbiddenException.class, () -> {
+            postService.deletePost(post.getId());
+        });
+    }
+}
