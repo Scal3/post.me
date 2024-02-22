0f24d73e1491b0f0cf02c862536100b17c161e6d
Herman <cool.goga501@gmail.com>
2024-02-22 14:50:11 +0100

    upd/add List<String> tags to CreatePostDto feat/add custom @ItemInListSize validator

--- src/main/java/com/herman/postme/post/dto/CreatePostDto.java
+++ src/main/java/com/herman/postme/post/dto/CreatePostDto.java
@@ -1,12 +1,15 @@
 package com.herman.postme.post.dto;
 
+import com.herman.postme.post.validation.ItemInListSize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -22,4 +25,9 @@ public class CreatePostDto {
     @NotBlank
     @Size(min = 10, max = 255)
     private String text;
+
+    @NotEmpty
+    @Size(min = 1, max = 3)
+    @ItemInListSize(min = 2, max = 50)
+    private List<String> tags;
 }
