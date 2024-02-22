b324c3aed55baf997787791356abcb9665a82b64
Herman <cool.goga501@gmail.com>
2024-02-22 15:25:49 +0100

    upd/add List<TagDto> tags to PostDto feat/add TagDto

--- src/main/java/com/herman/postme/post/dto/PostDto.java
+++ src/main/java/com/herman/postme/post/dto/PostDto.java
@@ -1,11 +1,13 @@
 package com.herman.postme.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.herman.postme.tag.dto.TagDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -25,6 +27,8 @@ public class PostDto {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    private List<TagDto> tags;
 
     @JsonProperty("isUpdated")
     public boolean getIsUpdated() {
---
+++ src/main/java/com/herman/postme/tag/dto/TagDto.java
@@ -0,0 +1,13 @@
+package com.herman.postme.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagDto {
+
+    private String name;
+}
