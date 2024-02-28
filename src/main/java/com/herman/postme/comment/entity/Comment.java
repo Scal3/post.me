aa4609edb689414cd5e744f6b9e0320366e7161c
Herman <cool.goga501@gmail.com>
2024-02-28 21:01:34 +0100

    upd/update Comment entity

--- src/main/java/com/herman/postme/comment/entity/Comment.java
+++ src/main/java/com/herman/postme/comment/entity/Comment.java
@@ -1,25 +1,22 @@
 package com.herman.postme.comment.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.herman.postme.post.entity.Post;
 import javax.persistence.*;
+import com.herman.postme.user.entity.User;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
 @Data
+@NoArgsConstructor
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(
-            nullable = false,
-            length = 55)
-    private String username;
 
     @Column(
             nullable = false,
@@ -32,16 +29,10 @@ public class Comment {
     private LocalDateTime createdAt;
 
     @ManyToOne
-    @JoinColumn(name = "posts_id")
-    @JsonIgnore
+    @JoinColumn(name = "post_id")
     private Post post;
 
-    public Comment() {}
-
-    public Comment(String username, String text, LocalDateTime createdAt, Post post) {
-        this.username = username;
-        this.text = text;
-        this.createdAt = createdAt;
-        this.post = post;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
