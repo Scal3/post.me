4ede790dd8e7ba7139bce00e2ab72eb4a63108b9
Herman <cool.goga501@gmail.com>
2024-03-02 01:16:35 +0100

    feat/add UserControllerAccessTest

--- src/test/java/com/herman/postme/user/service/UserServiceTest.java
+++ src/test/java/com/herman/postme/user/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import com.herman.postme.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
