284b815d2a21a02d96b0ec11afb74c9627d0635f
Herman <cool.goga501@gmail.com>
2024-03-07 16:12:38 +0100

    fix/remove all @Param annotations and use ? instead in repo

--- src/main/java/com/herman/postme/comment/repository/CommentRepository.java
+++ src/main/java/com/herman/postme/comment/repository/CommentRepository.java
@@ -4,30 +4,29 @@ import com.herman.postme.comment.entity.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByPostId(@Param("postId") long postId, Pageable pageable);
+    List<Comment> findAllByPostId(long postId, Pageable pageable);
 
     @Query("SELECT c " +
             "FROM Comment c " +
             "LEFT JOIN FETCH c.rates r " +
-            "WHERE c.id = :commentId")
-    Optional<Comment> findByIdWithLikes(@Param("commentId") long commentId);
+            "WHERE c.id = ?1")
+    Optional<Comment> findByIdWithLikes(long commentId);
 
     @Query("SELECT c, (SELECT COALESCE(SUM(cr.rate), 0) FROM CommentRate cr WHERE cr.comment = c) AS totalRate " +
             "FROM Comment c " +
-            "WHERE c.post.id = :postId " +
+            "WHERE c.post.id = ?1 " +
             "ORDER BY totalRate DESC")
-    List<Comment> findAllByPostIdOrderByLikesDesc(@Param("postId") long postId, Pageable pageable);
+    List<Comment> findAllByPostIdOrderByLikesDesc(long postId, Pageable pageable);
 
     @Query("SELECT c, (SELECT COALESCE(SUM(cr.rate), 0) FROM CommentRate cr WHERE cr.comment = c) AS totalRate " +
             "FROM Comment c " +
-            "WHERE c.post.id = :postId " +
+            "WHERE c.post.id = ?1 " +
             "ORDER BY totalRate ASC")
-    List<Comment> findAllByPostIdOrderByLikesAsc(@Param("postId") long postId, Pageable pageable);
+    List<Comment> findAllByPostIdOrderByLikesAsc(long postId, Pageable pageable);
 }
--- src/main/java/com/herman/postme/post/repository/PostRepository.java
+++ src/main/java/com/herman/postme/post/repository/PostRepository.java
@@ -4,7 +4,6 @@ import com.herman.postme.post.entity.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,52 +15,52 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY p.createdAt DESC")
-    List<Post> findAllByTagsOrderByCreatedAtDesc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByCreatedAtDesc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY p.createdAt ASC")
-    List<Post> findAllByTagsOrderByCreatedAtAsc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByCreatedAtAsc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
             "LEFT JOIN p.comments c " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY COUNT(c) DESC")
-    List<Post> findAllByTagsOrderByCommentsDesc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByCommentsDesc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
             "LEFT JOIN p.comments c " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY COUNT(c) ASC")
-    List<Post> findAllByTagsOrderByCommentsAsc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByCommentsAsc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p, (SELECT COALESCE(SUM(pr.rate), 0) FROM PostRate pr WHERE pr.post = p) AS totalRate " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY totalRate DESC")
-    List<Post> findAllByTagsOrderByLikesDesc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByLikesDesc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p, (SELECT COALESCE(SUM(pr.rate), 0) FROM PostRate pr WHERE pr.post = p) AS totalRate " +
             "FROM Post p " +
             "INNER JOIN p.tags t " +
-            "WHERE t.name IN :tags " +
+            "WHERE t.name IN ?1 " +
             "GROUP BY p.id " +
             "ORDER BY totalRate ASC")
-    List<Post> findAllByTagsOrderByLikesAsc(@Param("tags") List<String> tags, Pageable pageable);
+    List<Post> findAllByTagsOrderByLikesAsc(List<String> tags, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
@@ -87,39 +86,39 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "ORDER BY totalRate ASC")
     List<Post> findAllOrderByLikesAsc(Pageable pageable);
 
-    List<Post> findAllByUserId(@Param("userId") long userId, Pageable pageable);
+    List<Post> findAllByUserId(long userId, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "LEFT JOIN p.comments c " +
-            "WHERE p.user.id = :userId " +
+            "WHERE p.user.id = ?1 " +
             "GROUP BY p.id " +
             "ORDER BY COUNT(c) DESC")
-    List<Post> findAllByUserIdOrderByCommentCountDesc(@Param("userId") long userId, Pageable pageable);
+    List<Post> findAllByUserIdOrderByCommentCountDesc(long userId, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "LEFT JOIN p.comments c " +
-            "WHERE p.user.id = :userId " +
+            "WHERE p.user.id = ?1 " +
             "GROUP BY p.id " +
             "ORDER BY COUNT(c) ASC")
-    List<Post> findAllByUserIdOrderByCommentCountAsc(@Param("userId") long userId, Pageable pageable);
+    List<Post> findAllByUserIdOrderByCommentCountAsc(long userId, Pageable pageable);
 
     @Query("SELECT p, (SELECT COALESCE(SUM(pr.rate), 0) FROM PostRate pr WHERE pr.post = p) AS totalRate " +
             "FROM Post p " +
-            "WHERE p.user.id = :userId " +
+            "WHERE p.user.id = ?1 " +
             "ORDER BY totalRate DESC")
-    List<Post> findAllByUserIdOrderByLikesDesc(@Param("userId") long userId, Pageable pageable);
+    List<Post> findAllByUserIdOrderByLikesDesc(long userId, Pageable pageable);
 
     @Query("SELECT p, (SELECT COALESCE(SUM(pr.rate), 0) FROM PostRate pr WHERE pr.post = p) AS totalRate " +
             "FROM Post p " +
-            "WHERE p.user.id = :userId " +
+            "WHERE p.user.id = ?1 " +
             "ORDER BY totalRate ASC")
-    List<Post> findAllByUserIdOrderByLikesAsc(@Param("userId") long userId, Pageable pageable);
+    List<Post> findAllByUserIdOrderByLikesAsc(long userId, Pageable pageable);
 
     @Query("SELECT p " +
             "FROM Post p " +
             "LEFT JOIN FETCH p.rates r " +
-            "WHERE p.id = :postId")
-    Optional<Post> findByIdWithLikes(@Param("postId") long postId);
+            "WHERE p.id = ?1")
+    Optional<Post> findByIdWithLikes(long postId);
 }
